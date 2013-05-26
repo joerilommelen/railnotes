750b310caec83026ea93c28cdd3295f964e41673
joerilommelen <lommelen.joeri@gmail.com>
2013-05-26 17:05:34 +0200

    26052013
    aanmaak + design

---
+++ instellingen.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Railnotes</title>
+		<link rel="stylesheet" href="css/reset.css" type="text/css" media="screen" charset="utf-8">
+		<link rel="stylesheet" href="css/style.css" type="text/css" media="screen" charset="utf-8">
+		<link href="images/apple-touch-icon-precomposed.png" rel="apple-touch-icon-precomposed" />
+		<link href="images/apple-touch-icon-72x72-precomposed.png" rel="apple-touch-icon-precomposed" sizes="72x72" />
+		<link href="images/apple-touch-icon-114x114-precomposed.png" rel="apple-touch-icon-precomposed" sizes="114x114" />
+		<link href="images/apple-touch-icon-144x144-precomposed.png" rel="apple-touch-icon-precomposed" sizes="144x144" />
+		<meta content="text/html; charset=iso-8859-1" http-equiv="Content-Type" />
+		<meta name="viewport" content="user-scalable=0, initial-scale=0.5">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	</head>
+	<body>
+	<div class="container">
+		<div class="header">
+		
+		
+		</div>
+	
+	
+		
+		<div class="footer">
+		<a href="index.php"><img src="images/overzicht.png" alt="overzicht_active"></a>
+		<a href="zoeken.php"><img src="images/zoeken.png" alt="zoeken"></a>
+		<a href="instellingen.php"><img src="images/instellingen_active.png" alt="instellingen"></a>
+		</div>
+	</div>
+	</body>
+</html>
---
+++ overzicht.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Railnotes</title>
+		<link rel="stylesheet" href="css/reset.css" type="text/css" media="screen" charset="utf-8">
+		<link rel="stylesheet" href="css/style.css" type="text/css" media="screen" charset="utf-8">
+		<link href="images/apple-touch-icon-precomposed.png" rel="apple-touch-icon-precomposed" />
+		<link href="images/apple-touch-icon-72x72-precomposed.png" rel="apple-touch-icon-precomposed" sizes="72x72" />
+		<link href="images/apple-touch-icon-114x114-precomposed.png" rel="apple-touch-icon-precomposed" sizes="114x114" />
+		<link href="images/apple-touch-icon-144x144-precomposed.png" rel="apple-touch-icon-precomposed" sizes="144x144" />
+		<meta content="text/html; charset=iso-8859-1" http-equiv="Content-Type" />
+		<meta name="viewport" content="user-scalable=0, initial-scale=0.5">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+		<script src="javascript/functions.js" type="text/javascript"></script>
+		<!-- iPhone LAUNCHSCREEN-->
+		<link href="images/apple-launch-480h.png" sizes="320x480" media="(device-height: 480px)" rel="apple-touch-startup-image">
+		<!-- iPhone (Retina) LAUNCHSCREEN-->
+		<link href="images/apple-launch-480h2X.png" sizes="640x920" media="(device-height: 480px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image">
+		<!-- iPhone 5+ LAUNCHSCREEN-->
+		<link href="images/apple-launch-568h.png" sizes="640x1136" media="(device-height: 568px)" rel="apple-touch-startup-image">
+	</head>
+	<body>
+	<div class="container">
+		<div class="header">
+		
+		
+		</div>
+	
+	
+		
+		<div class="footer">
+		<a href="index.php"><img src="images/overzicht_active.png" alt="overzicht_active"></a>
+		<a href="zoeken.php"><img src="images/zoeken.png" alt="zoeken"></a>
+		<a href="instellingen.php"><img src="images/instellingen.png" alt="instellingen"></a>
+		</div>
+	</div>
+	</body>
+</html>
