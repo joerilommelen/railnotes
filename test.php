12a12fcf3034ff1e5976b498b1ec533bca55ce75
Laurens Nicolay <Laurens.nicolay@telenet.be>
2013-06-01 16:55:54 +0200

    010613

---
+++ test.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Railnotes</title>
+		<link rel="stylesheet" href="css/reset.css" type="text/css" media="screen" charset="utf-8">
+		<link rel="stylesheet" href="css/style.css" type="text/css" media="screen" charset="utf-8">
+		<link href="images/apple-touch-icon-precomposed.png" rel="apple-touch-icon-precomposed" />
+		<link href="images/apple-touch-icon-72x72-precomposed.png" rel="apple-touch-icon-precomposed" sizes="72x72" />
+		<link href="images/apple-touch-icon-114x114-precomposed.png" rel="apple-touch-icon-precomposed" sizes="114x114" />
+		<link href="images/apple-touch-icon-144x144-precomposed.png" rel="apple-touch-icon-precomposed" sizes="144x144" />
+		<meta content="text/html; charset=iso-8859-1" http-equiv="Content-Type" />
+		<meta name="viewport" content="user-scalable=0, initial-scale=0.5">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+		<script src="javascript/functions.js" type="text/javascript"></script>
+	</head>
+	<body>
+	<div class="container">
+		<div class="header">
+		
+		
+		</div>
+	
+	
+		
+		<div class="footer">
+		<a href="index.php"><img src="images/overzicht.png" alt="overzicht_active"></a>
+		<a href="zoeken.php"><img src="images/zoeken_active.png" alt="zoeken"></a>
+		<a href="instellingen.php"><img src="images/instellingen.png" alt="instellingen"></a>
+		</div>
+	</div>
+	</body>
+</html>
